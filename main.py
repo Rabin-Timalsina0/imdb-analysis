37d1f60d0bf21a582112947504d7842e1c3c2b46
rabintimalsina <167679006+Rabin-Timalsina0@users.noreply.github.com>
2026-01-06 15:57:55 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import pickle
+from tensorflow import keras
+
+app = FastAPI()
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+with open("tokenizer.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+
+model = keras.models.load_model("trained_model.keras")
+
+@app.get("/")
+async def home(request:Request):
+    return templates.TemplateResponse("index.html",{'request':request})
+
+@app.post("/predict")
+async def predict(request: Request, data: str = Form(...)):
+    sequences = tokenizer.texts_to_sequences([data])
+    padded_sequence = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=200)
+    prediction = model.predict(padded_sequence)
+    score = prediction[0][0]
+    label = "positive" if score > 0.5 else "negative"
+    return templates.TemplateResponse("index.html", {"request": request, "prediction": label})
